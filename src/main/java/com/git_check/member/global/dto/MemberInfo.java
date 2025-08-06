a4e0c50459fe5fe8e4b53d39b0e535754a567b45
bokyeong-Lee <kyeong8139@gmail.com>
2025-08-06 11:11:40 +0900

    fix: 빌드 오류 수정

--- src/main/java/com/git_check/member/global/dto/MemberInfo.java
+++ src/main/java/com/git_check/member/global/dto/MemberInfo.java
@@ -1,5 +1,15 @@
-package com.git_check.member.member;
+package com.git_check.member.global.dto;
 
-public class Member {
-    
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfo {
+    String socialLoginType;
+    String socialLoginId;
 }
